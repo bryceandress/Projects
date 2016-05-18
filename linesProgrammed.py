52ce6c62274acb9d348965762e413acb570dd491
bryceandress <andressbryce@gmail.com>
2016-05-18 15:56:08 -0400

    Writing lines to website:

--- linesProgrammed.py
+++ linesProgrammed.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 programmed = 0
 
@@ -11,7 +12,9 @@ for root, dirs, files in os.walk("/home/bryce/Documents/Projects/python_projects
 					programmed += 1
 #TODO
 #WRITE PROGRAMMED TO HIDDEN FILE
-f = open("/home/bryce/.lines", "w")
-f.write(str(programmed)+"\n")
+f = open("/home/bryce/Documents/Projects/bryceandress.github.io/_data/lines.yml", "w")
+f.write("- name: ")
+f.write(str(programmed))
+f.write(" lines of code committed as of " + str(datetime.date.today()))
 f.close()
 
